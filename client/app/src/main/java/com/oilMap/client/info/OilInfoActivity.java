07cef436878bae86a2940f07106d5e1ff19067cc
Francis Kim <kims_0121@naver.com>
2015-05-16 15:26:33 +0900

    로그인시 첫페이지 변경

---
+++ client/app/src/main/java/com/oilMap/client/info/OilInfoActivity.java
@@ -0,0 +1,61 @@
+package com.oilMap.client.info;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.cocosw.bottomsheet.BottomSheet;
+import com.oilMap.client.MainActivity;
+import com.oilMap.client.R;
+import com.oilMap.client.bluetooth.Bluetooth_reception;
+import com.oilMap.client.util.BackPressCloseHandler;
+
+public class OilInfoActivity extends Activity {
+
+    private BottomSheet bottomSheet;
+
+    private BackPressCloseHandler backPressCloseHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //Remove notification bar
+        setContentView(R.layout.activity_oil_info);
+
+        // ~ BottomSheet
+        bottomSheet = new BottomSheet.Builder(this, R.style.BottomSheet_StyleDialog).title("Option").sheet(R.menu.list).listener(new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case R.id.setting:
+                        Intent blue_exe = new Intent(OilInfoActivity.this, Bluetooth_reception.class);
+                        blue_exe.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        break;
+
+                    case R.id.logout:
+                        SharedPreferences pref = OilInfoActivity.this.getSharedPreferences("userInfo", 0);
+                        SharedPreferences.Editor prefEdit = pref.edit();
+                        prefEdit.putString("id","");
+                        prefEdit.commit();
+
+                        Intent idCheck = new Intent(OilInfoActivity.this, MainActivity.class);
+                        startActivity(idCheck);
+                        OilInfoActivity.this.finish();
+                        break;
+                }
+            }
+        }).build();
+
+        this.backPressCloseHandler = new BackPressCloseHandler(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.backPressCloseHandler.onBackPressed(this.bottomSheet);
+    }
+}
