5fda5178a0246bf31bc0c63111207a6b47733132
Francis Kim <kims_0121@naver.com>
2015-05-16 21:09:24 +0900

    후킹데이터 입력 부분 서버 추가

---
+++ server/src/main/java/com/oilMap/server/bill/FuelBillServiceImpl.java
@@ -0,0 +1,21 @@
+package com.oilMap.server.bill;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Francis on 2015-05-16.
+ */
+@Service
+public class FuelBillServiceImpl extends SqlSessionDaoSupport implements FuelBillService{
+
+    @Override
+    public void insert(FuelBill fuelBill) {
+        getSqlSession().insert("fuelBill.insert", fuelBill);
+    }
+
+    @Override
+    public FuelBill selectMaxFuelBill(String id) {
+        return getSqlSession().selectOne("fuelBIll.selectMaxFuelBill", id);
+    }
+}
