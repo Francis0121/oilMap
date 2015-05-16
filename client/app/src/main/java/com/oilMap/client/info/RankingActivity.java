a2e610a1130d543cd0d63271069aa9196529b32f
KimHyeonJun <ghost4425@naver.com>
2015-05-17 00:02:39 +0900

    리스트뷰 클릭시 지도 연동

--- client/app/src/main/java/com/oilMap/client/info/RankingActivity.java
+++ client/app/src/main/java/com/oilMap/client/info/RankingActivity.java
@@ -1,10 +1,14 @@
 package com.oilMap.client.info;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.oilMap.client.R;
+import com.oilMap.client.gps.MapsActivity;
 
 import java.util.ArrayList;
 
@@ -21,29 +25,31 @@ public class RankingActivity extends Activity {
 
         ArrayList<RankingItem> data = new ArrayList<>();
 
-        RankingItem a = new RankingItem(R.drawable.ic_icon,"a", R.drawable.ic_icon, "3.14");
-        RankingItem b = new RankingItem(R.drawable.ic_icon, "b", R.drawable.ic_icon, "3.14");
-        RankingItem c = new RankingItem(R.drawable.ic_icon, "c", R.drawable.ic_icon, "3.14");
+        RankingItem a = new RankingItem(R.drawable.ic_icon,"a", R.drawable.ic_icon, "Efficiency " + " 3.14" + "km/L");
+        RankingItem b = new RankingItem(R.drawable.ic_icon, "b", R.drawable.ic_icon, "Efficiency " + " 3.14" + "km/L");
+        RankingItem c = new RankingItem(R.drawable.ic_icon, "c", R.drawable.ic_icon, "Efficiency " + " 3.14" + "km/L");
 
         data.add(a);
         data.add(b);
         data.add(c);
-
-
         //////////////////////////////////////////////////////////////////////////////
 
-        RankingItem a_a = new RankingItem(R.drawable.ic_icon, "a_a", R.drawable.ic_icon, "3.14");
-        RankingItem b_b = new RankingItem(R.drawable.ic_icon, "b_b", R.drawable.ic_icon, "3.14");
-        RankingItem c_c = new RankingItem(R.drawable.ic_icon, "c_c", R.drawable.ic_icon, "3.14");
-        RankingItem d_d = new RankingItem(R.drawable.ic_icon, "d_d", R.drawable.ic_icon, "3.14");
+        RankingItem[] rankingArray = new RankingItem[20];
 
-        data.add(a_a);
-        data.add(b_b);
-        data.add(c_c);
-        data.add(d_d);
+        for(int i = 0; i < 20; i++){
+            rankingArray[i] = new RankingItem(R.drawable.ic_icon, "a_a", R.drawable.ic_icon,"Efficiency " + " 3.14" + "km/L");
+            data.add(rankingArray[i]);
+        }
 
         RankingviewAdapter adapter = new RankingviewAdapter(this, R.layout.ranking_item, data);
         listView.setAdapter(adapter);
-    }
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent map = new Intent(RankingActivity.this, MapsActivity.class);
+                startActivity(map);
+            }
+        });
+    }
 }
