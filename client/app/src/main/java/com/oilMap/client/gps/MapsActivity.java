73aae4daf4fc1621650b649408c2c3de269a6418
Francis Kim <kims_0121@naver.com>
2015-05-25 14:27:13 +0900

    처음에만 현재 위치로 이동되도록 변경

--- client/app/src/main/java/com/oilMap/client/gps/MapsActivity.java
+++ client/app/src/main/java/com/oilMap/client/gps/MapsActivity.java
@@ -23,7 +23,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.oilMap.client.R;
 
-public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapClickListener{
+public class MapsActivity extends FragmentActivity{
 
     private static final String TAG = "MapsActivity";
 
@@ -50,9 +50,6 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapCli
         mGoogleMap = ((SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map)).getMap();
 
-        // 맵의 이동
-        //mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
-
         GpsInfo gps = new GpsInfo(MapsActivity.this);
         // GPS 사용유무 가져오기
         if (gps.isGetLocation()) {
@@ -69,45 +66,19 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapCli
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void setUpMapIfNeeded(LatLng latLng) {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mGoogleMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            FragmentManager fm = getFragmentManager();
-            mGoogleMap = ((MapFragment) fm.findFragmentById(R.id.map)).getMap();
-            mGoogleMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14.0f));
-        }
-    }
-
-    @Override
-    public void onMapClick(LatLng point) {
-        // 현재 위도와 경도에서 화면 포인트를 알려준다
-        Point screenPt = mGoogleMap.getProjection().toScreenLocation(point);
-
-        // 현재 화면에 찍힌 포인트로 부터 위도와 경도를 알려준다.
-        LatLng latLng = mGoogleMap.getProjection().fromScreenLocation(screenPt);
-
-        Log.d("맵좌표", "좌표: 위도(" + String.valueOf(point.latitude) + "), 경도("
-                + String.valueOf(point.longitude) + ")");
-        Log.d("화면좌표", "화면좌표: X(" + String.valueOf(screenPt.x) + "), Y("
-                + String.valueOf(screenPt.y) + ")");
-    }
+    private Boolean isFirstTime = true;
 
     private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
         @Override
         public void onMyLocationChange(Location location) {
             Log.d(TAG, "Call on my location change listener");
             LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-            MarkerOptions option = new MarkerOptions();
-            option.position(loc);// 위도 • 경도
-            option.icon(BitmapDescriptorFactory.fromResource(R.drawable.curr_position));
-
             new MapsOtherAsyncTask(MapsActivity.this, mGoogleMap, id).execute(loc);
-//            mGoogleMap.addMarker(option);
             if(mGoogleMap != null){
-                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+                if(isFirstTime) {
+                    mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+                    isFirstTime = false;
+                }
             }
         }
     };
